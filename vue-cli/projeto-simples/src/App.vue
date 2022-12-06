5e9e31cf2d1c1fe1221cddae6b0e630a595b2c15
AndreMTS <andre.matos@sisloc.com.br>
2022-12-06 11:48:31 -0300

    120- Cominicação entre componentes

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>Ola Mundo</div>
+  <hr>
+  <Contador> </Contador>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+export default {
+    name: 'App',
+    components: {Contador}
+}
+</script>
+'
+
+<style>
+div{
+  font-size:50px;
+}
+</style>
---
+++ vue-cli/projeto-simples/src/components/Contador.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>Contador: {{contador}}</p>
+        <button @click="adicionar">+</button>        
+        <button @click="subtrair">-</button>        
+    </div>    
+</template>
+
+<script>
+export default {
+    name:'Contador',
+    data(){
+        return {
+            contador:0
+        }
+    },
+    methods:{
+        adicionar(){
+            this.contador++
+        },
+        subtrair(){
+            this.contador--
+        }
+    }    
+}
+</script>
+
+<style>
+
+</style>
